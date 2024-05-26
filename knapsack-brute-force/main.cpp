4b51e3215bb7961cadc20a26e69e62a93a491a82
bskrobich <bartskrobich@gmail.com>
2024-05-26 17:43:04 +0200

    Implemented readInput function and added sample data file

--- knapsack-brute-force/main.cpp
+++ knapsack-brute-force/main.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <chrono>
+#include <tuple>
+
+using namespace std;
+using namespace std::chrono;
+
+struct Item {
+    int weight;
+    int value;
+};
+
+pair<int, vector<Item>> readInput(const string& filename) {
+    ifstream file(filename);
+    int totalCapacity;
+    vector<Item> items;
+
+    if (!file.is_open()) {
+        cerr << "Unable to open file: " << filename << endl;
+    }
+    file >> totalCapacity;
+
+    int weight, value;
+    while (file >> weight >> value) {
+        items.push_back({weight, value});
+    }
+
+    file.close();
+    return {totalCapacity, items};
+}
 
 int main() {
+    string filename;
+    cout << "Enter data file absolute path: ";
+    cin >> filename;
+    auto [totalCapacity, items] = readInput(filename);
+
+    cout << "Total capacity: " << totalCapacity << endl;
+    cout << "Number of items: " << items.size() << endl;
 
 }
